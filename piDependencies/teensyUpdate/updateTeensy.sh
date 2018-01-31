fbb8f73e0fb86654bd1a3263e8535a0b9fa1cf24
Nicolas Pare <nicolaspare313@gmail.com>
2018-01-31 17:08:06 -0500

    update dependencies updater

--- piDependencies/teensyUpdate/updateTeensy.sh
+++ piDependencies/teensyUpdate/updateTeensy.sh
@@ -6,10 +6,14 @@ sketch=$1
 
 rm -r src/*
 
-if [$1 == ""] 
-	then
-		curl -H "Authorization: token $gitToken" -o src/AlveoleArduino.ino https://raw.githubusercontent.com/ProjetPropolis/AlveoleArduino/migrationSensor/AlveoleArduino.ino
-		echo "script AlveoleArduino has been update"
+if [ $1 = "hexgroup" ]; then
+	curl -H "Authorization: token $gitToken" -o src/AlveoleArduino.ino https://raw.githubusercontent.com/ProjetPropolis/AlveoleArduino/migrationSensor/AlveoleArduino.ino
+	echo "script AlveoleArduino has been update"
+
+elif [ $1 = "atomgroup" ]; then
+	curl -H "Authorization: token $gitToken" -o src/AlveoleArduino.ino https://raw.githubusercontent.com/ProjetPropolis/AlveoleArduino/molecules/AlveoleArduino.ino
+	echo "script MoleculeArduino has been update"
+
 else
 	curl -H "Authorization: token $gitToken" -o src/$sketch.ino https://raw.githubusercontent.com/ProjetPropolis/AlveoleArduino/migrationSensor/sketchTest/$sketch/$sketch.ino
 	echo "script $sketch has been update"
@@ -17,6 +21,7 @@ fi
 
 rm -rf src/__MACOSX
 
+cd /home/pi/Documents/teensyUpdate
 platformio run -t upload
 
 echo "project has been compiled"
