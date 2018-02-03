9c669f6a43d770e1fa21ce13945290528a55e50d
Nicolas Pare <nicolaspare313@gmail.com>
2018-02-03 15:17:58 -0500

    change teensyUpdate zip file

--- piDependencies/teensyUpdate/updateTeensy.sh
+++ piDependencies/teensyUpdate/updateTeensy.sh
@@ -2,18 +2,28 @@
 # Update teensy code for the propolis project
 
 gitToken=$GIT_KEY
+defaultValue=$MODULE_TYPE
+echo "defaultValue"
 sketch=$1
 
 rm -r src/*
 
-if [ $1 = "hexgroup" ]; then
+if [ "$1" = "hexgroup" ]; then
 	curl -H "Authorization: token $gitToken" -o src/AlveoleArduino.ino https://raw.githubusercontent.com/ProjetPropolis/AlveoleArduino/migrationSensor/AlveoleArduino.ino
 	echo "script AlveoleArduino has been update"
 
-elif [ $1 = "atomgroup" ]; then
+elif [ "$1" = "atomgroup" ]; then
 	curl -H "Authorization: token $gitToken" -o src/AlveoleArduino.ino https://raw.githubusercontent.com/ProjetPropolis/AlveoleArduino/molecules/AlveoleArduino.ino
 	echo "script MoleculeArduino has been update"
 
+elif [ "$1" = "default"]; then
+	if [ defaultValue = "hexgroup" ]; then
+		curl -H "Authorization: token $gitToken" -o src/AlveoleArduino.ino https://raw.githubusercontent.com/ProjetPropolis/AlveoleArduino/migrationSensor/AlveoleArduino.ino
+		echo "script AlveoleArduino has been update"
+	elif [ defaultValue = "atomgroup" ]; then
+		curl -H "Authorization: token $gitToken" -o src/AlveoleArduino.ino https://raw.githubusercontent.com/ProjetPropolis/AlveoleArduino/molecules/AlveoleArduino.ino
+		echo "script MoleculeArduino has been update"
+	fi
 else
 	curl -H "Authorization: token $gitToken" -o src/$sketch.ino https://raw.githubusercontent.com/ProjetPropolis/AlveoleArduino/migrationSensor/sketchTest/$sketch/$sketch.ino
 	echo "script $sketch has been update"
@@ -22,6 +32,8 @@ fi
 rm -rf src/__MACOSX
 
 cd /home/pi/Documents/teensyUpdate
-platformio run -t upload
+sudo platformio run -t upload &&
 
 echo "project has been compiled"
+
+read
